3c514b41d3dc6e5fe3d5ff18c8f3be0793cf8328
Akshat-iitk <akshatt21@iitk.ac.in>
2024-06-13 22:34:37 +0530

    initiated server files with GraphQl

--- Twitter/twitter-client/pages/index.tsx
+++ Twitter/twitter-client/pages/index.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { BsBell, BsBookmark, BsEnvelope, BsTwitter, BsTwitterX } from "react-icons/bs";
+import {
+  BsBell,
+  BsBookmark,
+  BsEnvelope,
+  BsTwitter,
+  BsTwitterX,
+} from "react-icons/bs";
 import React from "react";
 import { BiHash, BiHomeCircle, BiSolidHome, BiUser } from "react-icons/bi";
 const inter = Inter({ subsets: ["latin"] });
@@ -17,51 +23,53 @@ const sideBarMenuItems: TwitterSideButton[] = [
   },
   {
     title: "Explore",
-    icon: <BiHash/>,
+    icon: <BiHash />,
   },
   {
     title: "Notification",
-    icon: <BsBell/>,
+    icon: <BsBell />,
   },
   {
     title: "Message",
-    icon: <BsEnvelope/>,
+    icon: <BsEnvelope />,
   },
   {
     title: "Bookmarks",
-    icon: <BsBookmark/>,
+    icon: <BsBookmark />,
   },
   {
     title: "Profile",
-    icon: <BiUser/>,
+    icon: <BiUser />,
   },
 ];
 export default function Home() {
   return (
     <div className={inter.className}>
-    <div className=" grid grid-cols-12 h-screen w-screen px-52">
-      <div className="col-span-3 pt-1">
-        <div className=" h-fit w-fit text-3xl p-3 transition-all hover:bg-gray-900   hover:cursor-pointer  rounded-full">
-          <BsTwitterX />
+      <div className=" grid grid-cols-12 h-screen w-screen px-52">
+        <div className="col-span-3 pt-1">
+          <div className=" h-fit w-fit text-3xl p-3 transition-all hover:bg-gray-900   hover:cursor-pointer  rounded-full">
+            <BsTwitterX />
+          </div>
+          <div className="mt-4 text-xl  w-fit pr-4">
+            <ul>
+              {sideBarMenuItems.map((item) => (
+                <li
+                  className="flex justify-start items-center gap-4 hover:bg-gray-900 cursor-pointer rounded-full px-5 py-2 mt-2"
+                  key={item.title}
+                >
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <button className="bg-[#1d9bf0] hover:cursor-pointer hover:opacity-95 w-full mt-5 py-2 px-1 rounded-full mx-4 font-semibold">
+              Post
+            </button>
+          </div>
         </div>
-        <div className="mt-4 text-xl  w-fit pr-4">
-          <ul>
-            {sideBarMenuItems.map((item) => (
-              <li
-                className="flex justify-start items-center gap-4 hover:bg-gray-900 cursor-pointer rounded-full px-5 py-2 mt-2"
-                key={item.title}
-              >
-                <span>{item.icon}</span>
-                <span>{item.title}</span>
-              </li>
-            ))}
-          </ul>
-          <button className="bg-[#1d9bf0] hover:cursor-pointer hover:opacity-95 w-full mt-5 py-2 px-1 rounded-full mx-4 font-semibold">Post</button>
-        </div>
+        <div className="col-span-6 border-r-[1px] border-l-[1px] border-white"></div>
+        <div className="col-span-3"></div>
       </div>
-      <div className="col-span-6 border-r-[1px] border-l-[1px] border-white"></div>
-      <div className="col-span-3"></div>
-    </div>
     </div>
   );
 }
